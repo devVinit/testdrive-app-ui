eeb535e08c1065fb34fb31752c10c19e7a4ff147
Vinit <vinit.kumar@binaryinformatics.com>
2019-08-17 17:54:52 +0530

    simple car template is done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Header, Card, Image } from 'semantic-ui-react';
 import './App.css';
 
 function App() {
+  const [cars, setCars] = useState([]);
+
+  useEffect(() => {
+    axios.get('car')
+      .then(response => {
+        setCars(response.data);
+      }).catch(error => {
+        console.log(error);
+      })
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header as='h3' dividing>
+        Test Drive App
+      </Header>
+      <div className="flex-row">
+        {
+          cars && cars.length > 0 && cars.map(car => (
+            <div key={car.id}>
+              <Card>
+                <Image src={car.image} wrapped size='medium' />
+                <Card.Content>
+                  <Card.Header>{car.companyName} </Card.Header>
+                  <Card.Meta>
+                    <span className='date'>{car.modelName}</span>
+                  </Card.Meta>
+                </Card.Content>
+              </Card>
+            </div>
+          ))
+        }
+      </div>
     </div>
   );
 }
