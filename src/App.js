41950a2750ac8cdc69a394fe88052a3c73a9699e
Vinit <vinit.kumar@binaryinformatics.com>
2019-08-17 23:29:10 +0530

    booking done

--- src/App.js
+++ src/App.js
@@ -1,19 +1,53 @@
 import React, { useEffect, useState } from 'react';
+import { Header, Card, Image, Button, Modal, Input } from 'semantic-ui-react';
 import axios from 'axios';
-import { Header, Card, Image } from 'semantic-ui-react';
 import './App.css';
 
 function App() {
   const [cars, setCars] = useState([]);
+  const [carId, setCarId] = useState(null);
+  const [slot, setSlot] = useState(null);
+  const [schedule, setSchedule] = useState([]);
+  const [personName, setPersonName] = useState('');
 
   useEffect(() => {
+    getAvailaleCars();
+  }, []);
+
+  const getAvailaleCars = () => {
     axios.get('car')
       .then(response => {
         setCars(response.data);
       }).catch(error => {
         console.log(error);
-      })
-  }, []);
+      });
+  }
+
+  const getAvaialbleSlots = (id) => {
+    axios.get(`schedule/${id}`)
+      .then(response => {
+        setCarId(id);
+        setSchedule(response.data);
+      }).catch(error => {
+        console.log(error);
+      });
+  }
+
+  const createBooking = () => {
+    const booking = {
+      carId,
+      slot,
+      personName
+    }
+
+    axios.post('/schedule', booking)
+      .then(resonse => {
+        console.log(resonse);
+        setSlot(null);
+        setPersonName(null);
+        getAvaialbleSlots(carId);
+      }).catch(error => console.log(error));
+  }
 
   return (
     <div className="App">
@@ -24,10 +58,10 @@ function App() {
         {
           cars && cars.length > 0 && cars.map(car => (
             <div key={car.id}>
-              <Card>
+              <Card link onClick={() => getAvaialbleSlots(car.id)}>
                 <Image src={car.image} wrapped size='medium' />
                 <Card.Content>
-                  <Card.Header>{car.companyName} </Card.Header>
+                  <Card.Header>{car.companyName}</Card.Header>
                   <Card.Meta>
                     <span className='date'>{car.modelName}</span>
                   </Card.Meta>
@@ -37,6 +71,37 @@ function App() {
           ))
         }
       </div>
+
+      {
+        carId &&
+        <div>
+          {
+            schedule && schedule.length > 0 && schedule.map((slot, index) => (
+              <Button
+                key={index}
+                basic
+                color={(slot.isBooked) ? 'red' : 'green'}
+                disabled={slot.isPassed}
+                onClick={() => setSlot(slot.time)}>
+                {slot.time}
+              </Button>
+            ))
+          }
+        </div>
+      }
+
+      {
+        slot &&
+        <Modal open={slot} onClose={() => setSlot(null)} closeOnDocumentClick>
+          <Modal.Header>Select a Photo</Modal.Header>
+          <Modal.Content>
+            <Input size='large' placeholder='Booked By' onChange={event => setPersonName(event.target.value)} value={personName} />
+            <Button onClick={() => createBooking()}>
+              Create
+            </Button>
+          </Modal.Content>
+        </Modal>
+      }
     </div>
   );
 }
